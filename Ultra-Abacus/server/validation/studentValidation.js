2b6a94b171804c054b756aab50faa106947d36a7
Aryanshinde2121 <shindearyan2121@gmail.com>
2026-06-25 13:00:20 +0530

    Removed duplicate Ultra-Abacus folder

--- Ultra-Abacus/server/validation/studentValidation.js
+++
@@ -1,7 +0,0 @@
-import { body } from "express-validator";
-
-export const createStudentValidation = [
-  body("name").notEmpty().withMessage("Name is required"),
-  body("email").isEmail().withMessage("Valid email is required"),
-  body("rollNo").notEmpty().withMessage("Roll number is required"),
-];
